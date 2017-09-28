be4554795239d7507090d1018a3f11c25f3396c6
Aliiiya <katty.09.ciusan@gmail.com>
2017-09-28 11:38:21 +0300

    first

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m, k;
+            m = int.Parse(Console.ReadLine());
+            k = int.Parse(Console.ReadLine());
+            while ((m != 0) && (k != 0))
+            {
+                if (m > k)
+                {
+                    m = m % k;
+                }
+                else
+                {
+                    k = k % m;
+                }
+            }
+            Console.WriteLine(k + m);
+            Console.ReadLine();
+        }
+    }
+}
