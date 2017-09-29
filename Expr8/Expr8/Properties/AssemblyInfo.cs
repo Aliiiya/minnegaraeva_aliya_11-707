bd5a03d287ef9332189f0d5a4518796313fc2c15
Aliiiya <katty.09.ciusan@gmail.com>
2017-09-29 09:05:04 +0300

    Expr8

--- Expr8/Expr8/Properties/AssemblyInfo.cs
+++ Expr8/Expr8/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("ConsoleApp1")]
+[assembly: AssemblyTitle("Expr8")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApp1")]
+[assembly: AssemblyProduct("Expr8")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
-[assembly: Guid("2d25cc73-4eeb-4404-a00b-d31f70e676f7")]
+[assembly: Guid("29575eee-5f40-4120-9b58-9ca6a5cc03a4")]
 
 // Сведения о версии сборки состоят из следующих четырех значений:
 //
