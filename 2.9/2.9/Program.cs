6f8011ec4882a7c037fb67d34dcc1e266f806f91
Aliiiya <katty.09.ciusan@gmail.com>
2017-10-12 16:53:02 +0300

    semestr

---
+++ 2.9/2.9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k = 0;
+            Console.WriteLine("Введите число");
+            n = int.Parse(Console.ReadLine());
+            while (n > 0)
+            {
+                if (n % 2 == 1)
+                {
+                    k = k + 1;
+                }
+                n = n / 2;
+            }
+            Console.WriteLine(k);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 3.20/3.20/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = -1, sEven = 0, sOdd = 0;
+            int kEven = 0, kOdd = 0;
+            while (n != 0)
+            {
+                n = double.Parse(Console.ReadLine());
+                if (n % 2 == 0)
+                {
+                    sEven += n;
+                    ++kEven;                   
+                }
+                else
+                {
+                    sOdd += n;
+                    ++kOdd;
+                }
+            }
+            if (kEven > kOdd)
+                Console.WriteLine(sEven);
+            else Console.WriteLine(sOdd);
+            Console.ReadLine();
+        }
+    }
+}
