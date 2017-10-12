23d2266ce4058b4fdd6583334e76e69b965a3bc4
Aliiiya <katty.09.ciusan@gmail.com>
2017-10-12 18:56:57 +0300

    semestr 4.16

---
+++ 4.16/4.16/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4._16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, k = 0, kMax = 0, k1 = 0, s = 0;
+            n = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= n; i++)
+            {
+                for (int m = i; s <= n; m++)
+                {
+                    s += m;
+                    k1++;
+                    if (s == n)
+                        k = k1;
+                }
+                if (k > kMax)
+                    kMax = k;
+                s = 0;
+                k1 = 0;
+            }
+            Console.WriteLine(kMax);
+            Console.ReadLine();
+        }
+    }
+}
