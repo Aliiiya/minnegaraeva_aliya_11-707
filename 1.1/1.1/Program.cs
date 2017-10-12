bd0c4d75a0678dee5b30fda1d2363b4ee72b3bc2
Aliiiya <katty.09.ciusan@gmail.com>
2017-10-12 16:48:01 +0300

    semestr 1.1

---
+++ 1.1/1.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            string a;
+            Console.WriteLine("Введите координаты шахматной доски");
+            i = int.Parse(Console.ReadLine());
+            a = Console.ReadLine();
+            if (i % 2 == 1)
+                if ((a == "a") || (a == "c") || (a == "e") || (a == "g"))
+                    Console.WriteLine("BLACK");
+                else Console.WriteLine("WHITE");
+            else
+            {
+                if ((a == "a") || (a == "c") || (a == "e") || (a == "g"))
+                    Console.WriteLine("WHITE");
+                else Console.WriteLine("BLACK");
+            }
+            Console.ReadLine();
+        }
+    }
+}
