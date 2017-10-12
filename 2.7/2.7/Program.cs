eedcec9874fada8ddbf3df15ec91e0e077e30eaa
Aliiiya <katty.09.ciusan@gmail.com>
2017-10-12 16:36:46 +0300

    vol2.7

---
+++ 2.7/2.7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, d, sk = 0, ks, s = 0;
+            for (i = 1; i <= 100; i++)
+            {
+                sk += i * i;
+                s += i;
+            }
+            ks = s * s;
+            d = sk - ks;
+            Console.WriteLine(d);
+            Console.ReadLine();
+        }
+    }
+}
