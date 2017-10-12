a2b28d926c4e020f946bb25081acd7f1ead87777
Aliiiya <katty.09.ciusan@gmail.com>
2017-10-12 18:29:13 +0300

    semestr 3.1

---
+++ 3.1/3.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, a, b, a1, b1, nok;
+            Console.WriteLine("Введите количество чисел");
+            n = int.Parse(Console.ReadLine());
+            a = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                b = int.Parse(Console.ReadLine());
+                a1 = a;
+                b1 = b;
+                while (a != b)
+                {
+                    if (a > b)
+                        a = a - b;
+                    else
+                        b = b - a;
+                }
+                nok = a1 * b1 / a;
+                a = nok;
+            }
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
